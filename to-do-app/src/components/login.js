895af91c9c3fd6f912b7314306dd5f2cf399b319
Ronil Patel <ronil.star111@gmail.com>
2023-07-30 19:57:58 -0300

    Updated the Login page UI

--- to-do-app/src/components/login.js
+++ to-do-app/src/components/login.js
@@ -43,7 +43,9 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>Welcome to the To-do List World!</h1>
+    <div style={formContainerStyle}>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
         <div>
@@ -53,6 +55,7 @@ const LoginPage = () => {
             name="email"
             value={formData.email}
             onChange={handleChange}
+            style={inputStyle1}
             required
           />
         </div>
@@ -63,13 +66,67 @@ const LoginPage = () => {
             name="password"
             value={formData.password}
             onChange={handleChange}
+            style={inputStyle}
             required
           />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" style={loginButtonStyle}>Login</button>
       </form>
     </div>
+    </div>
   );
+};
+
+
+const headingStyle = {
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  marginTop: '30px',
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column', // Changed from 'row' to 'column'
+  alignItems: 'center',
+  minHeight: '100vh',
+  background: '#f7f7f7',
+};
+
+const inputStyle = {
+  padding: '10px',
+  borderRadius: '4px',
+  border: '1px solid #ddd',
+  marginTop: '10px',
+  marginBottom: '10px',
+  alignItems: 'center',
+  width: '65%',
+  marginLeft: '10px',
+};
+
+const inputStyle1 = {
+  ...inputStyle,
+  marginLeft: '40px',
+};
+
+// CSS styles for the form container
+const formContainerStyle = {
+  width: '350px',
+  padding: '30px',
+  border: '1px solid #ddd',
+  borderRadius: '8px',
+  background: '#fff',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+};
+
+const loginButtonStyle = {
+  width: '100%', // Make the button broad with the size of the box
+  padding: '10px', // Optional: Add padding to the button
+  background: 'green', // Green background color
+  color: '#fff', // White text color
+  border: 'none', // Remove button border
+  borderRadius: '4px', // Add border-radius for rounded corners
+  cursor: 'pointer', // Show pointer cursor on hover
+  marginTop: '10px',
 };
 
 export default LoginPage;
